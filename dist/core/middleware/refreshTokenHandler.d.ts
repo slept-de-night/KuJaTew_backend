484ce818d66f44dcf5a9dc98c93ee9316baf970a
HolyGotty <warut.mass@gmail.com>
2025-09-04 12:56:26 +0700

    Delete Dist

--- dist/core/middleware/refreshTokenHandler.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const refreshTokenHandler: import("express").RequestHandler<import("express-serve-static-core").ParamsDictionary, any, any, import("qs").ParsedQs, Record<string, any>>;
-//# sourceMappingURL=refreshTokenHandler.d.ts.map
