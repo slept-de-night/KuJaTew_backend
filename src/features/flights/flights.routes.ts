6a7ba8a65ef35f922339d98c1395ea279abf90a1
ApplJu1ce <earn9kanis@gmail.com>
2025-09-14 17:59:31 +0700

    Fixed Flight + Tested Flight, No more error, Finally working as intended | HOORAY git add .

--- src/features/flights/flights.routes.ts
+++ src/features/flights/flights.routes.ts
@@ -3,8 +3,8 @@ import * as controller from "./flights.controller";
 
 export const flightRouter = Router();
 
-flightRouter.post("/:trip_id/flights", controller.post_flight) //Done || Not tested yet
-flightRouter.get("/:trip_id/flights", controller.get_flight) //Done || Not tested yet
+flightRouter.post("/:trip_id/flights", controller.post_flight) //Tested
+flightRouter.get("/:trip_id/flights", controller.get_flight) //Tested
 
-flightRouter.put("/:trip_id/flights/:flight_id", controller.put_flight) //Done || Not tested yet
-flightRouter.delete("/:trip_id/flights/:flight_id", controller.delete_flight) //Done || Not tested yet
+flightRouter.put("/:trip_id/flights/:flight_id", controller.put_flight) //Tested
+flightRouter.delete("/:trip_id/flights/:flight_id", controller.delete_flight) //Tested
