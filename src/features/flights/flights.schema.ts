7301ee52cfc5e2c2a687dea5d24b2b8efabef57a
ApplJu1ce <earn9kanis@gmail.com>
2025-10-04 22:02:52 +0700

    Change doc on get_flight

--- src/features/flights/flights.schema.ts
+++ src/features/flights/flights.schema.ts
@@ -51,13 +51,13 @@ export type CreateFlightBody = z.infer<typeof CreateFlightBodySchema>;
 //swagger
 export const FlightItemSchema = z.object({
   flight_id: z.number().int().openapi({ example: 123 }),
-  dep_date: z.string().openapi({ example: "01/09/2025" }),
-  dep_time: z.string().openapi({ example: "08:45" }),
+  dep_date: z.string().openapi({ example: "2025-08-31T17:00:00.000Z" }),
+  dep_time: z.string().openapi({ example: "08:45:00" }),
   dep_country: z.string().openapi({ example: "Thailand" }),
   dep_airp_code: z.string().openapi({ example: "BKK" }),
 
-  arr_date: z.string().openapi({ example: "02/09/2025" }),
-  arr_time: z.string().openapi({ example: "12:30" }),
+  arr_date: z.string().openapi({ example: "2025-09-01T17:00:00.000Z" }),
+  arr_time: z.string().openapi({ example: "12:30:00" }),
   arr_country: z.string().openapi({ example: "Japan" }),
   arr_airp_code: z.string().openapi({ example: "HND" }),
 
