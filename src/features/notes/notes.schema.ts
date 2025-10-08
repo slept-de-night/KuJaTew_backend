9d2a7cb0bf52d36e2353619d6ce30b4d6d9c639f
Keennaja <keen13814@gmail.com>
2025-10-08 21:57:11 +0700

    update activity note

--- src/features/notes/notes.schema.ts
+++ src/features/notes/notes.schema.ts
@@ -1,19 +1,20 @@
 import { z } from "zod";
 
 export const noteintripschema = z.object({
-  nit_id: z.number().int(),
+  nit_id: z.coerce.number().int(),
   note: z.string(),
   name: z.string(),
   user_id: z.string(),
   profile_picture_path: z.string().nullable(),
-  is_editable: z.number().int(),
+  is_editable: z.coerce.number().int(),
   note_time: z.coerce.date(),
 });
 
 export const noteactivityschema = z.object({
-  pnote_id: z.number().int(),
+  pnote_id: z.coerce.number().int(),
+  pit_id: z.coerce.number().int(),
   note: z.string(),
   name: z.string(),
   profile_picture_path: z.string().nullable(),
-  is_editable: z.number().int()
+  is_editable: z.coerce.number().int()
 });
